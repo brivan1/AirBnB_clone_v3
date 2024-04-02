ff404a7b170d09fffb08364482a43491983c17d6
Clevoh <clevelihasi.com>
2024-04-02 08:57:21 -0700

    task 3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,10 @@
 This module contains the blueprint for the API.
 """
 from flask import Blueprint
+
+app_views = Blueprint('app_view', __name__, url_prefix="/api/v1/views")
+
+# Import views from their respective modules
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -11,5 +15,3 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-
-app_views = Blueprint('app_view', __name__, url_prefix="/api/v1/views")
